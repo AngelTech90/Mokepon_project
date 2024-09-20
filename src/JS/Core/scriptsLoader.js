e2c2b47c19a1e4686924822abb66305773d67ad3
Ange_Tech(Angel_Molina) <angelmolinaimas@gmail.com>
2024-09-20 11:11:13 -0400

    Fixing for let the functional page to publish

--- src/JS/Core/scriptsLoader.js
+++ src/JS/Core/scriptsLoader.js
@@ -1,19 +1,19 @@
 
-const script1 = document.createElement('script');
-const script2 = document.createElement('script');
-const script3 = document.createElement('script');
-const script4 = document.createElement('script');
-const script5 = document.createElement('script');
-const script6 = document.createElement('script');
-const script7 = document.createElement('script');
-const script8 = document.createElement('script');
-const script9 = document.createElement('script');
-const script10 = document.createElement('script');
-const script11 = document.createElement('script');
-const script12 = document.createElement('script');
-const script13 = document.createElement('script');
-const script14 = document.createElement('script');
-const script15 = document.createElement('script');
+const script1 = document.createElement("script");
+const script2 = document.createElement("script");
+const script3 = document.createElement("script");
+const script4 = document.createElement("script");
+const script5 = document.createElement("script");
+const script6 = document.createElement("script");
+const script7 = document.createElement("script");
+const script8 = document.createElement("script");
+const script9 = document.createElement("script");
+const script10 = document.createElement("script");
+const script11 = document.createElement("script");
+const script12 = document.createElement("script");
+const script13 = document.createElement("script");
+const script14 = document.createElement("script");
+const script15 = document.createElement("script");
 
 script1.defer = true;
 script2.defer = true;
@@ -31,21 +31,21 @@ script13.defer = true;
 script14.defer = true;
 script15.defer = true;
 
-script1.src = './src/JS/Logic/Functions/global_functions.js';
-script2.src = './src/JS/Logic/global/combat_vars.js';
-script3.src = './src/JS/Logic/global/map_vars.js';
-script4.src = './src/JS/Logic/Data_structures/combat_data_structures.js';
-script5.src = './src/JS/Logic/Data_structures/global_data_structures.js';
-script6.src = './src/JS/Logic/Data_structures/map_data_structures.js';
-script7.src = './src/JS/Logic/Classes/global_classes.js';
-script8.src = './src/JS/Logic/Objects/global_objects.js';
-script9.src = './src/JS/Logic/Functions/combat_functions.js';
-script10.src = './src/JS/Logic/Functions/render_functions.js';
-script11.src = './src/JS/Logic/Functions/map_functions.js';
-script12.src = './src/JS/Logic/Functions/information_functions.js';
-script13.src = './src/JS/Animations/map_animations.js';
-script14.src = './src/JS/Animations/set_map_enemies.js';
-script15.src = './src/JS/Core/main.js';
+script1.src = "./src/JS/Logic/Functions/global_functions.js";
+script2.src = "./src/JS/Logic/global/combat_vars.js";
+script3.src = "./src/JS/Logic/global/map_vars.js";
+script4.src = "./src/JS/Logic/Data_structures/combat_data_structures.js";
+script5.src = "./src/JS/Logic/Data_structures/global_data_structures.js";
+script6.src = "./src/JS/Logic/Data_structures/map_data_structures.js";
+script7.src = "./src/JS/Logic/Classes/global_classes.js";
+script8.src = "./src/JS/Logic/Objects/global_objects.js";
+script9.src = "./src/JS/Logic/Functions/combat_functions.js";
+script10.src = "./src/JS/Logic/Functions/render_functions.js";
+script11.src = "./src/JS/Logic/Functions/map_functions.js";
+script12.src = "./src/JS/Logic/Functions/information_functions.js";
+script13.src = "./src/JS/Animations/map_animations.js";
+script14.src = "./src/JS/Animations/set_map_enemies.js";
+script15.src = "./src/JS/Core/main.js";
 
 const sorteredScripts = [
 
@@ -67,7 +67,7 @@ const sorteredScripts = [
 
 ]
 
-let mainHead = document.getElementById('head');
+let mainHead = document.getElementById("head");
 
 for (let i = 0; i < sorteredScripts.length; i++) {
     
